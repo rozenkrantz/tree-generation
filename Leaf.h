df9d3eccf2f4d109be1a48b536c2e0a42c1a7e7c
Doro Umarov <umarov.doro@gmail.com>
2018-10-17 15:03:48 +0300

    Update Leaf.h
    Remove uneccessary function, indent

--- Leaf.h
+++ Leaf.h
@@ -1,30 +1,27 @@
 #ifndef LEAF_H
 #define LEAF_H
 
+#include "Vector.h"
+
 namespace dequinox
 {
-	struct Leaf
-	{
-            bool reached{false};
-            Vector pos{0,0};
+    struct Leaf
+    {
+        bool reached{false};
+        Vector pos;
 
-            Leaf(Vector pos) : pos(pos) {}
-            	
-		void show(Bitmap &image)
-		{
-                	image.setPixel(pos.x, pos.y, 255, 255, 255);
-            }
-        
-	    	void gotReached()
-		{
-            	reached = true;
-            }
-        	
-		Vector getPos()
-		{
-        		return pos;
-        	}
-    	};
+        Leaf(Vector pos) : pos(pos) {}
+
+        void gotReached()
+        {
+            reached = true;
+        }
+
+        Vector getPos()
+        {
+            return pos;
+        }
+    };
 }
 
 #endif // LEAF_H
