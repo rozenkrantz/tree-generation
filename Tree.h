3ac2207e28aeb6951d83365db1df886b4f2ca479
Doro Umarov <umarov.doro@gmail.com>
2018-10-17 15:07:13 +0300

    Update Tree.h
    Remove uneccessary fields, change function

--- Tree.h
+++ Tree.h
@@ -1,37 +1,41 @@
 #ifndef TREE_H
 #define TREE_H
 
+#include <ctime>
 #include <cmath>
 #include <vector>
+#include <cstdlib>
+#include <fstream>
+
 #include "Leaf.h"
-#include "Vector.h"
 #include "Branch.h"
 #include "Bitmap.h"
 #include "BitmapInfoHeader.h"
 #include "BitmapFileHeader.h"
 
-#include <ctime>
-#include <cstdlib>
-
 namespace dequinox
 {
-	class Tree
-    	{
-      	private:
-		      int N{0};
-		      int D{0};
-		      std::vector<Branch> branches;
-		      std::vector<Leaf> leaves;
-		      Bitmap image;
-		      float max_dist{5000};
-		      float min_dist{1};
-    		public:
-        		Tree(int N, int D);
-			void generateLeaves();
-			void grow();
-        		void show();
-			~Tree();  		          	
-	};
+    class Tree
+    {
+        private:
+            int width{800};
+            int height{800};
+            float max_dist{100};
+            float min_dist{20};
+
+            Bitmap image;
+            std::vector<Branch> branches;
+            std::vector<Leaf> leaves;
+
+        public:
+            Tree(int min_dist, int max_dist, int width, int height);
+            ~Tree();
+
+            bool grow();
+            void show();
+            void generateLeaves(std::string filename);
+            void generateLeaves(int N);
+    };
 }
 
 #endif // TREE_H
